46cae40a1557f7aa020c5f8a1b7b257e88d49325
sydneythompson2 <sydneythompson2@utexas.edu>
2022-04-07 14:52:59 -0500

    created variables and initialized servo and button. i  think. im eating

--- rumpelstiltslay/rumpelstiltslay.ino
+++ rumpelstiltslay/rumpelstiltslay.ino
@@ -1,5 +1,23 @@
+/a
+AET rumpelstiltskin
+rumpelstiltslay
+team members: sydney thompson & casey dutton
+description: button and servo program
+
+a/
+
+//servo code library
+#include<Servo.h>;
+
+//variables
+
+Servo myServo;
+const int buttonPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
+myServo.attach(9);
+pinMode(buttonPin, INPUT);
 
 }
 
